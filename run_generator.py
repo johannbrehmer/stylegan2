713c8c10769a5bdd0e55b0dd360764e8dfa16a98
Johann Brehmer <mail@johannbrehmer.de>
2020-04-23 21:40:00 -0400

    Fixed another small bug

--- run_generator.py
+++ run_generator.py
@@ -38,7 +38,7 @@ def generate_images(network_pkl, seeds, truncation_psi, subspace=None, grid=Fals
             print('Generating image for latent vars %s (grid point %d/%d) ...' % (z_, grid_idx, len(seeds)))
             z = np.zeros((1, *Gs.input_shape[1:])) # [minibatch, component]
             z[:,:subspace] = z_
-            tflib.set_vars({var: np.zeros(*var.shape.as_list()) for var in noise_vars}) # [height, width]
+            tflib.set_vars({var: np.zeros(var.shape) for var in noise_vars}) # [height, width]
             images = Gs.run(z, None, **Gs_kwargs) # [minibatch, height, width, channel]
             PIL.Image.fromarray(images[0], 'RGB').save(dnnlib.make_run_dir_path('seed%04d.png' % grid_idx))
 
